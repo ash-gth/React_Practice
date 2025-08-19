f247206aacb01d9d835e3aaea8bc8ec9417e24ea
ash-gth <ashisharma9318@gmail.com>
2025-08-19 16:17:47 +0530

    Button Caption change by using useState

--- src/Components/LearnEvent.jsx
+++ src/Components/LearnEvent.jsx
@@ -1,16 +1,26 @@
-import React from "react" 
 
+import { useState } from "react"
 const LearnEvent = () => {
+        const [message, setMessage] = useState('');
+    
     const handleClick = () => {
-        console.log ("Clicked");
+        setMessage('Click')
+        
     }
+    const handleEmpty = () => {
+        setMessage('')
+        
+    }
+    
     const handleClickAgain = (param) => {
-        console.log (param);
+        setMessage(param)
     }
 
     return(
         <>
         <button onClick={handleClick}>Click Me </button>
+        <button onClick={handleEmpty}>Empty</button>
+        <h1>{message}</h1>
         <br />
         <button onClick={() => handleClickAgain("Clicked Me Again")}>Click Me Again</button>
         
