07bd1d609a08dce2a75a0c5a52f64e85d351b523
ash-gth <ashisharma9318@gmail.com>
2025-08-19 15:36:42 +0530

    Map function

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,37 @@ function App() {
      price:500,
     },
   } 
+  const list = [
+    {
+      name : "apple",
+      price : 300
+    },
+    {
+      name : "Tesla",
+      price : 900
+    },
+    {
+      name : "VI",
+      price : 100
+    },
+    
+  ]
   
   return (
     <>
       <h1>Learn React</h1> 
       <Learnjsx />
-      <LearnProps stock= 'Tesla' price= {price_1}/>
-      <PropsCard name = {data.stock1.name} price = {data.stock1.price}    />
+      {/* <LearnProps stock= 'Tesla' price= {price_1}/> */}
+      {/* <PropsCard name = {data.stock1.name} price = {data.stock1.price}    />
       <PropsCard name = {data.stock2.name} price = {data.stock2.price}    />
-      <PropsCard name = {data.stock3.name} price = {data.stock3.price}    />
+      <PropsCard name = {data.stock3.name} price = {data.stock3.price}    /> */}
+      {
+        list.map((stock, index) => (
+          <> 
+            <PropsCard name={stock.name} price={stock.price} key={index} />
+          </>
+        ))
+      }
       
       </>
   )
