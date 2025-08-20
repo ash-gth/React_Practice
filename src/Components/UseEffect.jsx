be04dce5027da15e9f047db0cd67648d1a60f1e1
ash-gth <ashisharma9318@gmail.com>
2025-08-20 16:46:20 +0530

    Clean Up Function using useState

--- src/Components/UseEffect.jsx
+++ src/Components/UseEffect.jsx
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const UseEffect = () => {
-    const [count, setCount] = useState(0)
-        const increment = () => {
-        setCount((prev) => prev + 1  );
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const [random, setRandomNum] = useState(0);
 
-    }
-    const [random, setRandomNum] = useState(0);
+  const randomNum = () => {
+    const randomNum = Math.floor(Math.random() * 10);
+    setRandomNum(randomNum);
+  };
 
-        const randomNum = () => {
-        const randomNum= Math.floor(Math.random() * 10)
-        setRandomNum(randomNum);
+  useEffect(() => {
+    console.log("Mounted")
+    return () => console.log("CleanFunction is called ");
+  }, [count]);
 
-    }
-
-    useEffect(()=> {console.log("Mounted")}, [count] )
   return (
     <>
-        <h2> Count : {count}</h2>
-        <button onClick={() => increment()}>Increase Me</button>
-        <br />
-        <h2>Random Number: {random}</h2>
-        <button onClick={() => randomNum()}>Random Number</button>
-    
+      <h2> Count : {count}</h2>
+      <button onClick={() => increment()}>Increase Me</button>
+      <br />
+      <h2>Random Number: {random}</h2>
+      <button onClick={() => randomNum()}>Random Number</button>
     </>
-  )
-}
+  );
+};
 
-export default UseEffect
+export default UseEffect;
